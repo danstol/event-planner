e312415ccd1b14434b90ff17d570634ae974eb56
danil <danil@stolaconsulting.com>
2019-01-16 14:21:50 -0600

    Adding signup page and view

--- event_planner/authentication/urls.py
+++ event_planner/authentication/urls.py
@@ -7,5 +7,6 @@ app_name = 'authentication'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='authentication/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='authentication/logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='authentication/logout.html'), name='logout'),
+    path('signup/', views.signup, name='signup')
 ]
--- event_planner/authentication/views.py
+++ event_planner/authentication/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 
+from .forms import SignUpForm
 
-def login(request):
-    return render(request, 'authentication/login.html', {})
+
+def signup(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'Your account was successfully created.')
+            return HttpResponseRedirect('/')
+        elif not form.is_valid():
+            messages.add_message(request, messages.WARNING,
+                                 'There were problems creating your account, please review the information given and'
+                                 ' try again')
+            return render(request, 'authentication/signup.html', {'form': form})
+    else:
+        form = SignUpForm()
+    return render(request, 'authentication/signup.html', {'form': form})
